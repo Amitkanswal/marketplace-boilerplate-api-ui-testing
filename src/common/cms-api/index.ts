83757d335e943f2f78a158f73aff4ffa3a31c4fb
swati.priya <swati.priya@contentstack.com>
2024-10-30 22:26:05 +0530

    feat: added api calls for stack & content type events

--- src/common/cms-api/index.ts
+++ src/common/cms-api/index.ts
@@ -1,12 +1,50 @@
 import UiLocation from "@contentstack/app-sdk/dist/src/uiLocation";
 import { ContentstackClient } from '@contentstack/management';
+import { singlepageCT, multiPageCT, schema } from './mock/content-type'
 
 const ContentstackAPI = {
-  fetchAllStacks: (client: ContentstackClient, apiSdk:UiLocation) => {
+
+  //----------------Organization----------------
+
+  fetchOrgDetails: (client: ContentstackClient, appSdk: UiLocation) => {
+    return client.organization().fetchAll();
+  },
+  //----------------Stack----------------
+  createStack: async (client: ContentstackClient, appSdk: UiLocation) => {
+    return client.stack().create({ name: 'New Stack', description: 'This is a new stack', master_locale: 'en-us' });
+  },
+
+  fetchStackById: (client: ContentstackClient, appSdk: UiLocation) => {
+    return client.stack({ api_key: appSdk.ids.apiKey }).fetch();
+  },
+  fetchAllStacks: (client: ContentstackClient, appSdk: UiLocation) => {
     return client.stack().query().find();
   },
-  fetchStackById: (client: ContentstackClient, appSdk:UiLocation) => {
-    return client.stack({ api_key: appSdk.ids.apiKey }).fetch();
+  //----------------Content Type----------------
+
+  CreateSinglePageContentType: (client: ContentstackClient, appSdk: UiLocation) => {
+    return client.stack({ api_key: appSdk.ids.apiKey }).contentType().create({ content_type: singlepageCT.content_type });
+  },
+  CreateMultiPageContentType: (client: ContentstackClient, appSdk: UiLocation) => {
+    return client.stack({ api_key: appSdk.ids.apiKey }).contentType().create({ content_type: multiPageCT.content_type });
+  },
+
+  fetchContentType: (client: ContentstackClient, appSdk: UiLocation) => {
+    return client.stack({ api_key: appSdk.ids.apiKey }).contentType(multiPageCT.content_type.uid).fetch();
+  },
+
+  fetchAllContentType: (client: ContentstackClient, appSdk: UiLocation) => {
+    return client.stack({ api_key: appSdk.ids.apiKey }).contentType().query().find();
+  },
+
+  queryContentTypeTitle: (client: ContentstackClient, appSdk: UiLocation) => {
+    return client.stack({ api_key: appSdk.ids.apiKey }).contentType(multiPageCT.content_type.title).fetch();
+  },
+  //----------------Entries----------------
+  
+  //----------------locale----------------
+  fetchLocale: (client: ContentstackClient, appSdk: UiLocation) => {
+    client.stack({ api_key: appSdk.ids.apiKey }).locale().query().find();
   },
 };
 
