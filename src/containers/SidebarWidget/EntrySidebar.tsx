150f6f5d0007995dc10542245e0d0f5a952cabe6
Abhishek Ezhava <abhishek.ezhava@contentstack.com>
2025-08-19 17:41:58 +0530

    feat: e2e getdraftdata test-case first commit

--- src/containers/SidebarWidget/EntrySidebar.tsx
+++ src/containers/SidebarWidget/EntrySidebar.tsx
@@ -8,55 +8,22 @@ import Icon from "../../assets/Entry-Sidebar-Logo.svg";
 import ReadOnly from "../../assets/lock.svg";
 import JsonView from "../../assets/JsonView.svg";
 import ConfigModal from "../../components/ConfigModal/ConfigModal";
+import { useAppSdk } from "../../common/hooks/useAppSdk";
+import { useExtensionEvents } from "../../common/hooks/useExtensionEvents";
+import { TestTableComponent } from "../../components/Table";
 
 const EntrySidebarExtension = () => {
   const appConfig = useAppConfig();
+  const appSDK = useAppSdk();
 
-  const [isRawConfigModalOpen, setRawConfigModalOpen] = useState<boolean>(false);
-
-  const handleViewRawConfig = useCallback(() => {
-    setRawConfigModalOpen(true);
-  }, []);
-
-  const handleCloseModal = useCallback(() => {
-    setRawConfigModalOpen(false);
-  }, []);
-
-  const sampleAppConfig = appConfig?.["sample_app_configuration"] || "";
-  const trimmedSampleAppConfig =
-    sampleAppConfig.length > 15 ? `${sampleAppConfig.substring(0, 15)}...` : sampleAppConfig;
+  const customFieldEvent = useExtensionEvents();
+  const [localState, setLocalState] = useState(customFieldEvent);
 
   return (
     <div className="layout-container">
       <div className="ui-location-wrapper">
         <div className="ui-location">
-          <div className="ui-container">
-            <div className="logo-container">
-              <img src={Icon} alt="Logo" />
-              <p title={localeTexts.SidebarWidget.title}>{localeTexts.SidebarWidget.title}</p>
-            </div>
-            <div className="config-container">
-              <div className="label-container">
-                <p className="label">Sample App Configuration</p>
-                <p className="info">(read only)</p>
-              </div>
-              <div className="input-wrapper">
-                <div className="input-container">
-                  <p className="config-value">{trimmedSampleAppConfig}</p>
-                  <img src={ReadOnly} alt="ReadOnlyLogo" />
-                </div>
-
-                <img src={JsonView} alt="Show-Json-CTA" className="show-json-cta" onClick={handleViewRawConfig} />
-                {isRawConfigModalOpen && appConfig && <ConfigModal config={appConfig} onClose={handleCloseModal} />}
-              </div>
-            </div>
-            <div className="location-description">
-              <p className="location-description-text">{parse(localeTexts.SidebarWidget.body)}</p>
-              <a target="_blank" rel="noreferrer" href={localeTexts.SidebarWidget.button.url}>
-                <span className="location-description-link">{localeTexts.SidebarWidget.button.text}</span>
-              </a>
-            </div>
-          </div>
+          <TestTableComponent initEventData={customFieldEvent} updatedEventData={localState}/>
         </div>
       </div>
     </div>
